2a8bfa9683eeddf0b1608b341c58e85828177e84
Jack Killelea <JackDKillelea@users.noreply.github.com>
2018-08-26 22:57:54 +0100

    Partially Completed
    Working camera, this will clamp to a certain zoom both in and out, you can also pan around the map. Prefabs for the models etc. Everything working that should be.

---
+++ City Builder/Assets/Scripts/Building.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+    public int id;
+    public int cost;
+    public string buildingName;
+}
---
+++ City Builder/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 mouseOriginPoint;
+    private Vector3 offset;
+    private bool dragging;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel")
+            * (Camera.main.orthographicSize), 2.5f, 50f);
+
+        if (Input.GetMouseButton(2))
+        {
+            offset = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            if (!dragging)
+            {
+                dragging = true;
+                mouseOriginPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            dragging = false;
+        }
+        if (dragging)
+            transform.position = mouseOriginPoint - offset;
+    }
+}
+
